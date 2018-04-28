3f3063233c1fd49bbdf8d6f2b076c1939205a1e4
Nicolas Cardozo <36283346+Nicolas-Cardozo@users.noreply.github.com>
2018-04-27 22:04:10 -0300

    Update models.py

--- app/models.py
+++ app/models.py
@@ -11,7 +11,7 @@ class Curso(models.Model):
     instituicao = models.CharField(max_length=200)
 class Vestibular(models.Model):
     nome = models.CharField(max_length=200)
-class Candidatos(models.Model):
+class Candidato(models.Model):
     nome = models.CharField(max_length=100)
     email = models.CharField(max_length=200)
 
